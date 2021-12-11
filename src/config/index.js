e4fa46de6389faa1065f6144dc295d5f1e69b967
dungpham <dungpham@dungphams-iMac-Pro.local>
2021-12-11 17:01:05 +0700

    cm

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,4 @@
-const END_POINT = 'https://haircutappapi20211210091501.azurewebsites.net'
+const END_POINT = 'https://985e-14-187-40-186.ngrok.io'
 const MAP_KEY = 'Xb03PqoGZdlN18gTGXXhEZI66cGueQI7s2lazEC7'
 const token = localStorage.getItem('token')
 
