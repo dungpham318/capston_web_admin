d9a1ba85c8090f84dd3e73b086bb57057fd51dce
dungpham <dungpham@dungphams-iMac-Pro.local>
2021-12-12 08:00:19 +0700

    cm

--- app.js
+++ app.js
@@ -2,10 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'build')));
-
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+const root = require("path").join(__dirname, "./build");
+app.use(express.static(root));
+app.get("*", (req, res) => {
+  res.sendFile("index.html", { root });
 });
-
 app.listen(9005)
