f167eec4bb72687b54fc54e35a6a35e8589b24d8
dungpham <dungpham@dungphams-iMac-Pro.local>
2021-12-12 09:31:06 +0700

    cm

---
+++ webpack.prod.js
@@ -0,0 +1,67 @@
+const common = require('./webpack.common');
+const { merge } = require('webpack-merge');
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CompressionPlugin = require('compression-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+// require('dotenv').config({ path: path.join(__dirname, '.env') });
+const Dotenv = require('dotenv-webpack');
+
+module.exports = merge(common, {
+  mode: 'production',
+  devtool: 'source-map',
+  entry: {
+    app: ['@babel/polyfill', './src/index.js'],
+  },
+  output: {
+    filename: '[name].[fullhash].js',
+    path: path.resolve(__dirname, 'build'),
+    // publicPath: '/sovico_medical_declaration/',
+  },
+  // optimization: {
+  //   runtimeChunk: false,
+  //   splitChunks: {
+  //     cacheGroups: {
+  //       default: false,
+  //       vendor: {
+  //         test: /[\\/]node_modules[\\/]/,
+  //         name: 'vendor',
+  //         chunks: 'all',
+  //       },
+
+  //     },
+  //   },
+  // },
+  optimization: {
+    minimize: true,
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CompressionPlugin({
+      test: /\.js(\?.*)?$/i,
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].[fullhash].css',
+    }),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'public', 'index.html'),
+      favicon: path.join(__dirname, 'public', 'favicon.ico'),
+      filename: 'index.html',
+    }),
+    new Dotenv({
+      path:
+        process.env.sovico === 'prod'
+          ? './.env.aws.prod'
+          : process.env.sovico === 'uat'
+            ? './.env.aws.uat'
+            : './.env.fis',
+    }),
+  ],
+  performance: {
+    hints: 'warning',
+    assetFilter: function (assetFilename) {
+      return assetFilename.endsWith('.js.gz');
+    },
+  },
+})
